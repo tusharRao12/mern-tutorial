73e1f424214cb25bb0a47c15de2ed34a276ba3b6
tusharrdx <141331956+tusharrdx@users.noreply.github.com>
2024-10-30 18:07:33 +0530

    image upload code

--- blog-application/controllers/postController.js
+++ blog-application/controllers/postController.js
@@ -1,3 +1,4 @@
+const File = require("../models/File")
 const Post = require("../models/Post")
 
 exports.getPosts = (req,res)=>{
@@ -10,28 +11,41 @@ exports.getPosts = (req,res)=>{
 exports.addPostsForm = (req,res)=>{
     res.render('post/addPost',{
         title:'Add Posts',
-        error:''
+        error:'',
+        success:''
     })
 }
 exports.addPosts = async (req, res) => {
     const { title, content } = req.body;
-
-    try {
-        if (!req.session.userId) {
-            return res.redirect('/auth/login');
-        }
-
-        const newPost = await Post.create({
-            title,
-            content,
-            author: req.session.userId
-        });
-
-        res.redirect('/post');
-    } catch (error) {
-        res.render("post/addPost", {
-            title: "Add Post",
-            error: error.message,
-        });
+    if(!req.files || req.files.lenght === 0){
+        return res.render('post/addPost',{
+            title:"Add Post",
+            error:"Image field is reuqired"
+        })
     }
+    const images = await Promise.all(req.files.map(async(file)=>{
+        const newFile = new File({
+            url:file.path,
+            public_id:file.filename,
+            uploaded_by:req.session.userId
+        });
+        await newFile.save();
+        console.log(newFile);
+        return {
+            url:newFile.url,
+            public_id:newFile.public_id,
+        }
+    }))
+
+    const newPost = new Post({
+        title,
+        content,
+        author:req.session.userId,
+        images,
+    });
+    await newPost.save();
+    res.render('post/addPost',{
+        title:'Posts',
+        success:"Post created Sucssesfuly"
+    });
 };
--- blog-application/routes/postRoutes.js
+++ blog-application/routes/postRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const postRoutes = express.Router();
 const {getPosts,addPostsForm,addPosts} = require('../controllers/postController');
+const upload = require('../config/multer');
 // const isAuthenticated = require('../middlewares/isAuthenticated');
 
 postRoutes.get('/', getPosts);
 
 postRoutes.get('/add',addPostsForm);
 
-postRoutes.post('/add', addPosts);
+postRoutes.post('/add',upload.array('images',5), addPosts);
 
 
 
