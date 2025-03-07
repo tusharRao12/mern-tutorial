6c776a59d607f6e7c32750aa93aaac24dedd4791
tusharrdx <141331956+tusharrdx@users.noreply.github.com>
2025-03-07 16:30:26 +0530

    css modules 4:06:37

--- react-by-prashant-sir/5.map-conditionrender-props/src/App.jsx
+++ react-by-prashant-sir/5.map-conditionrender-props/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   let foodItems = ["Apple", "Salad", "Milk", "Dal"]; 
   return (
     <>
-      <h1>Healthy Food</h1>
+      <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage items={foodItems} />
       <FoodItems items={foodItems} />
     </>
--- react-by-prashant-sir/5.map-conditionrender-props/src/components/Item.jsx
+++ react-by-prashant-sir/5.map-conditionrender-props/src/components/Item.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from "./Item.module.css";
 
 const Item = ({ foodItem }) => {
-  return <li className="list-group-item">{foodItem}</li>;
+  return (
+    <li className={`${styles["kg-item"]} list-group-item `}>
+      <span>{foodItem}</span>
+    </li>
+  );
 };
 
 export default Item;
