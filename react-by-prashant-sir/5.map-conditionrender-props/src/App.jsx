db5b14a9990e3754230704d8033d913ed0666afb
tusharrdx <tushar@rdxsolutions.in>
2025-03-14 17:07:35 +0530

    5:04:026

--- react-by-prashant-sir/5.map-conditionrender-props/src/App.jsx
+++ react-by-prashant-sir/5.map-conditionrender-props/src/App.jsx
@@ -1,3 +1,4 @@
+import Container from "./components/Container";
 import ErrorMessage from "./components/ErrorMessage";
 import FoodItems from "./components/FoodItems";
 
@@ -5,9 +6,14 @@ function App() {
   let foodItems = ["Apple", "Salad", "Milk", "Dal"]; 
   return (
     <>
+      <Container>
       <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage items={foodItems} />
       <FoodItems items={foodItems} />
+    </Container>
+    <Container>
+        <p>Above are the list of healthy foods</p>
+    </Container>
     </>
   );
 }
