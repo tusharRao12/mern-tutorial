a4573036ec6e012ba05144847e406b7dd987f377
tusharrdx <tushar@rdxsolutions.in>
2025-03-16 17:05:18 +0530

    6:22:22

--- react-by-prashant-sir/7.passing-fucntion-using-props-and-state-management/src/components/Item.jsx
+++ react-by-prashant-sir/7.passing-fucntion-using-props-and-state-management/src/components/Item.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from "./Item.module.css";
 
-const Item = ({ foodItem, handleBuyButton }) => {
+const Item = ({ foodItem, handleBuyButton, bought }) => {
   return (
-    <li className={`${styles["kg-item"]} list-group-item `}>
+    <li className={`${styles["kg-item"]} list-group-item ${bought && "active"}`}>
       <span>{foodItem}</span>
       <button
         className="btn btn-sm btn-info"
