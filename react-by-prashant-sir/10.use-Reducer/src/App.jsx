b8ebebd4d5e5aa3d4aa6c610b7b3993e4baa5b02
tusharrdx <141331956+tusharrdx@users.noreply.github.com>
2025-03-28 17:31:28 +0530

    9:43:00

--- react-by-prashant-sir/10.use-Reducer/src/App.jsx
+++ react-by-prashant-sir/10.use-Reducer/src/App.jsx
@@ -2,20 +2,45 @@ import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
 import "./App.css";
 import TodoItems from "./components/TodoItems";
-import { useState } from "react";
+import { useReducer } from "react";
 import WelcomeMessge from "./components/WelcomeMessge";
 
+const todoItemsReducer = (currTodoItems, action) =>{
+  let newTodoItems = currTodoItems;
+   if(action.type === 'NEW_ITEM'){
+    newTodoItems = [
+      ...currTodoItems,
+      {name:action.payload.itemName, dueDate:action.payload.dueDate},
+    ];
+   }else if (action.type === 'DELETE_ITEM'){
+    newTodoItems = currTodoItems.filter(
+      (item) => item.name !== action.payload.itemName
+    );
+   }
+  return newTodoItems;
+};
+
 function App() {
-  let [todoItems, setTodoItems] = useState([]);
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
 
-  const handleNewItem = (itemName,itemDueDate)=>{
-    const newTodoItems = [...todoItems,{name:itemName,dueDate:itemDueDate}];
-    setTodoItems(newTodoItems);
+  const handleNewItem = (itemName,itemDueDate) =>{
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload:{
+        itemName,
+        itemDueDate
+      }
+    };
+    dispatchTodoItems(newItemAction);
   }
-
   const handleDeleteItem = (todoItemName) =>{
-    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems);
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName:todoItemName
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
   }
   return (
     <center className="todo-container">
