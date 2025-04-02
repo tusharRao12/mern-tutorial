5c831a2d1a77d9e1da19135b990cdcf42b5e982e
tusharrdx <141331956+tusharrdx@users.noreply.github.com>
2025-04-02 18:30:15 +0530

    11:36:00

--- react-by-prashant-sir/4.projects/7.scoail-media/src/App.jsx
+++ react-by-prashant-sir/4.projects/7.scoail-media/src/App.jsx
@@ -2,16 +2,26 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import PostListProvider from "./store/posts-list-store";
+import { useState } from 'react';
 
 function App() {
+
+  const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
+    <PostListProvider>
     <div className="app-container">
-      <Sidebar />
+      <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <div className="content">
         <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
         <Footer />
       </div>
     </div>
+    </PostListProvider>
   );
 }
 
--- react-by-prashant-sir/4.projects/7.scoail-media/src/components/Sidebar.jsx
+++ react-by-prashant-sir/4.projects/7.scoail-media/src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({ selectedTab, setSelectedTab }) => {
+
   return (
     <div
-      className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark"
-      style={{ width: "280px" }}
+      className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
     >
       <a
         href="/"
@@ -17,44 +17,40 @@ const Sidebar = () => {
       </a>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <a href="#" className="nav-link active" aria-current="page">
+        <li
+          className="nav-item"
+          onClick={() => {
+            setSelectedTab("Home");
+          }}
+        >
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTab === "Home" && "active"
+            }`}
+            aria-current="page"
+          >
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#home"></use>
             </svg>
             Home
           </a>
         </li>
-        <li>
-          <a href="#" className="nav-link text-white">
+        <li
+          onClick={() => {
+            setSelectedTab("Create Post");
+          }}
+        >
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTab === "Create Post" && "active"
+            }`}
+          >
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#speedometer2"></use>
             </svg>
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <svg className="bi pe-none me-2" width="16" height="16">
-              <use xlinkHref="#table"></use>
-            </svg>
-            Orders
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <svg className="bi pe-none me-2" width="16" height="16">
-              <use xlinkHref="#grid"></use>
-            </svg>
-            Products
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <svg className="bi pe-none me-2" width="16" height="16">
-              <use xlinkHref="#people-circle"></use>
-            </svg>
-            Customers
+            Create Post
           </a>
         </li>
       </ul>
@@ -103,6 +99,6 @@ const Sidebar = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
