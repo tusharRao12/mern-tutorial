cf32c90cb7421392e6020b1894b515d7309ecc82
tusharrdx <141331956+tusharrdx@users.noreply.github.com>
2025-05-12 12:03:25 +0530

    complete mertn aut app

--- auth-mern-app/frontend/src/App.js
+++ auth-mern-app/frontend/src/App.js
@@ -3,14 +3,18 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 import './App.css';
+import { useState } from 'react';
+import RefreshHandler from './RefreshHandler';
 
 function App() {
   const [isAuthenticated,setIsAuthenticated] = useState(false);
   const PrivateRoute = ({element}) =>{
     return isAuthenticated ? element : <Navigate to = "/login"/>
   }
+
   return (
     <div className="App">
+      <RefreshHandler setIsAuthenticated ={setIsAuthenticated}/>
       <Routes>
         <Route path ='/' element={<Navigate to = '/login'/>}/>
         <Route path = '/login' element = {<Login/>}/>
